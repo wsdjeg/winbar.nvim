5adb8083e2166c72cee9b70ee0d6e7175b8edc8f
Eric Wong <wsdjeg@outlook.com>
2025-03-02 19:25:22 +0800

    skip nofile buftype

--- lua/winbar/init.lua
+++ lua/winbar/init.lua
@@ -9,65 +9,71 @@
 local M = {}
 
 local default_conf = {
-  winbar_seperator = '',
-  excluded_filetypes = {'defx'},
+	winbar_seperator = "",
+	excluded_filetypes = { "defx" },
 }
 
-local highlight = require('spacevim.api.vim.highlight')
+local highlight = require("spacevim.api.vim.highlight")
 local function def_colors()
-  local name = vim.g.colors_name or 'gruvbox'
+	local name = vim.g.colors_name or "gruvbox"
 
-  local t
+	local t
 
-  if vim.g.spacevim_custom_color_palette and #vim.g.spacevim_custom_color_palette > 0 then
-    t = vim.g.spacevim_custom_color_palette
-  else
-    local ok = pcall(function()
-      t = vim.fn['SpaceVim#mapping#guide#theme#' .. name .. '#palette']()
-    end)
+	if vim.g.spacevim_custom_color_palette and #vim.g.spacevim_custom_color_palette > 0 then
+		t = vim.g.spacevim_custom_color_palette
+	else
+		local ok = pcall(function()
+			t = vim.fn["SpaceVim#mapping#guide#theme#" .. name .. "#palette"]()
+		end)
 
-    if not ok then
-      t = vim.fn['SpaceVim#mapping#guide#theme#gruvbox#palette']()
-    end
-  end
-  vim.api.nvim_set_hl(0, 'SpaceVim_winbar', {
-    fg = t[2][1],
-    bg = t[2][2],
-    ctermfg = t[2][4],
-    ctermbg = t[2][3],
-  })
-  highlight.hi_separator('SpaceVim_winbar', 'Normal')
+		if not ok then
+			t = vim.fn["SpaceVim#mapping#guide#theme#gruvbox#palette"]()
+		end
+	end
+	vim.api.nvim_set_hl(0, "SpaceVim_winbar", {
+		fg = t[2][1],
+		bg = t[2][2],
+		ctermfg = t[2][4],
+		ctermbg = t[2][3],
+	})
+	highlight.hi_separator("SpaceVim_winbar", "Normal")
 end
 
 local function redraw_winbar()
-  local file_name = vim.fn.expand('%:t')
+	local file_name = vim.fn.expand("%:t")
 
-  if file_name == '' then
-    return
-  elseif vim.regex('^__Tagbar__'):match_str(file_name) then
-    file_name = 'Tagbar'
-  end
+	if file_name == "" then
+		return
+	elseif vim.o.buftype == "nofile" then
+		return
+	elseif vim.regex("^__Tagbar__"):match_str(file_name) then
+		file_name = "Tagbar"
+	end
 
-  local value = '%#SpaceVim_winbar#' .. file_name .. ' %#SpaceVim_winbar_Normal#' .. default_conf.winbar_seperator .. '%#Normal#'
+	local value = "%#SpaceVim_winbar#"
+		.. file_name
+		.. " %#SpaceVim_winbar_Normal#"
+		.. default_conf.winbar_seperator
+		.. "%#Normal#"
 
-  vim.api.nvim_set_option_value('winbar', value, { scope = 'local' })
+	vim.api.nvim_set_option_value("winbar", value, { scope = "local" })
 end
 
 function M.setup(opt)
-  if type(opt) == 'table' then
-    default_conf = vim.tbl_deep_extend('force', {}, default_conf, opt)
-  end
-  def_colors()
-  local augroup = vim.api.nvim_create_augroup('winbar.nvim', {
-    clear = true,
-  })
-  vim.api.nvim_create_autocmd({ 'BufWinEnter' }, {
-    group = augroup,
-    pattern = '*',
-    callback = function(e)
-      redraw_winbar()
-    end,
-  })
+	if type(opt) == "table" then
+		default_conf = vim.tbl_deep_extend("force", {}, default_conf, opt)
+	end
+	def_colors()
+	local augroup = vim.api.nvim_create_augroup("winbar.nvim", {
+		clear = true,
+	})
+	vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
+		group = augroup,
+		pattern = "*",
+		callback = function(e)
+			redraw_winbar()
+		end,
+	})
 end
 
 return M
